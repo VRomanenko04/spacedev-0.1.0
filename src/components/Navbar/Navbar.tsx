c956e612e7dcdd508a121e09d0e6fcb6b8f17408
VRomanenko04 <vovasan2004@gmail.com>
2024-01-27 16:13:35 +0200

    Установил sass, сделал Navbar, добавил в проект logo

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Navbar.module.scss';
+import Link from 'next/link';
+import Logo from '../../assets/spaceDev_logo.svg';
+import Image from 'next/image';
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <Link href='/'>
+                <Image src={Logo} alt='Logo'/>
+            </Link>
+            <div className={styles.right_side}>
+                <nav>
+                    <ul className={styles.links}>
+                        <li><Link className={styles.link} href='/courses'>courses</Link></li>
+                        <li><Link className={styles.link} href='/account'>account</Link></li>
+                    </ul>
+                </nav>
+                <div className={styles.avatar}></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
