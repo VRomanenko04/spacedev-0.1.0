22f03a2cf8d9abecd28450dc3952453e88056b31
VRomanenko04 <vovasan2004@gmail.com>
2024-01-27 23:20:40 +0200

    Сделал UI компонент Button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from './Button.module.scss';
 
-type Props = {}
+type Props = {
+    extraClass?: string
+    onClickFunc?: React.MouseEventHandler<HTMLButtonElement>
+    buttonText: string
+}
 
 const Button = (props: Props) => {
     return (
-        <div>Button</div>
+        <button className={`${styles.UiBtn} ${props.extraClass}`} onClick={props.onClickFunc}>
+            {props.buttonText}
+        </button>
     )
 }
 
-export default Button
+export default Button;
