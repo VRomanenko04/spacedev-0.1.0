ecae0605867889823017f1a9143b55d124d0612f
VRomanenko04 <vovasan2004@gmail.com>
2024-01-28 23:51:43 +0200

    Сделал компонент use client, сделал анимацию на inView

--- src/components/UI/TextParagraph/TextParagraph.tsx
+++ src/components/UI/TextParagraph/TextParagraph.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+'use client'
+import React, { useRef } from 'react';
 import styles from './TextParagraph.module.scss';
+import { motion, useInView } from 'framer-motion';
 
 type TextProps = {
     title: string
     text: string
-    animationClass?: string
 }
 
 const TextParagraph = (props: TextProps) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref);
+
+    const variants = {
+        hidden: { height: 0 },
+        visible: { height: 130 }
+    };
+
     return (
         <div className={styles.container}>
-            <div className={`${styles.bar} ${props.animationClass}`}></div>
+            <motion.div 
+                ref={ref}
+                className={`${styles.bar}`}
+                animate={isInView ? 'visible' : 'hidden'}
+                variants={variants}
+                transition={{ duration: 0.5 }}
+            ></motion.div>
             <div className={styles.content}>
                 <h2>{props.title}</h2>
                 <div dangerouslySetInnerHTML={{ __html: props.text }} />
