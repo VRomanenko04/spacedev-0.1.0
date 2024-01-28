913cc7b9105c7ddcda15123a45e84e643250475c
VRomanenko04 <vovasan2004@gmail.com>
2024-01-28 19:25:01 +0200

    Расширил возможности Button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -3,6 +3,7 @@ import styles from './Button.module.scss';
 
 type Props = {
     extraClass?: string
+    textAnimateClass?: string
     onClickFunc?: React.MouseEventHandler<HTMLButtonElement>
     buttonText: string
 }
@@ -10,7 +11,7 @@ type Props = {
 const Button = (props: Props) => {
     return (
         <button className={`${styles.UiBtn} ${props.extraClass}`} onClick={props.onClickFunc}>
-            {props.buttonText}
+            <p className={props.textAnimateClass}>{props.buttonText}</p>
         </button>
     )
 }
