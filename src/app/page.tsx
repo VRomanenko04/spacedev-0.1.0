1a14d9156458413985dfae9db48a88627f1ddadf
VRomanenko04 <vovasan2004@gmail.com>
2024-01-31 00:46:56 +0200

    Добавил Footer на HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HomeHeader from "@/components/HomeHeader/HomeHeader";
 import HomePlans from "@/components/HomePlans/HomePlans";
+import Footer from "@/components/UI/Footer/Footer";
 import WhySpacedev from "@/components/WhySpacedev/WhySpacedev";
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
         <WhySpacedev />
         <HomePlans />
       </main>
+      <Footer />
     </>
   );
 }
