e4b5d6471edef9992b5ac26c7368888bd24650ab
VRomanenko04 <vovasan2004@gmail.com>
2024-02-03 22:12:49 +0200

    Поставил заглушку на функцию формы

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import styles from './Login.module.scss';
-import Button from '@/components/UI/Button/Button';
 import Link from 'next/link';
-import Input from '@/components/Input/Input';
+import LoginForm from '@/components/LoginForm/LoginForm';
+
+export type LoginData = {
+    email: string
+    password: string
+}
 
 const LoginPage = () => {
+    const [loginData, setLoginData] = useState<LoginData>({
+        email: '',
+        password: ''
+    })
+
+    const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault
+        console.log(loginData);
+    }
+
     return (
         <main className={styles.wrapper}>
             <div className={styles.container}>
                 <h1 className={styles.title}>Log in</h1>
-                <form className={styles.form}>
-                    <div>
-                        <Input type='email' labetText='Email'/>
-                        <Input type='password' labetText='Password'/>
-                    </div>
-                    <Button buttonText='Submit' extraClass={styles.btn}/>
-                </form>
+                <LoginForm 
+                    formFunc={handleForm}
+                    formStates={setLoginData}
+                />
                 <div className={styles.no__acc}>
                     <p>No acc?</p>
                     <Link className={styles.link} href='/auth/register'>Register</Link>
