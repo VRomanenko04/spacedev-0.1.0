84f05e0f39d80a04b7a9ed7ace4a7150f1d21d47
VRomanenko04 <vovasan2004@gmail.com>
2024-02-03 22:42:48 +0200

    Модифицировал Input по функционалу

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Input.module.scss';
 
 type InputProps = {
     type: string
+    inputId: string
     labetText?: string
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    value: string
 }
 
 const Input = (props: InputProps) => {
     return (
         <label>
             <p className={styles.label__text}>{props.labetText}</p>
-            <input className={styles.input} type={props.type} />
+            <input 
+                className={styles.input} 
+                type={props.type} 
+                id={props.inputId} 
+                value={props.value}
+                onChange={props.handleChange}
+            />
         </label>
     )
 }
