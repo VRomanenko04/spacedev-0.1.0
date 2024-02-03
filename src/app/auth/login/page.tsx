3072110ef4d8e1ee8521a03d19b3057ea79c1f1d
VRomanenko04 <vovasan2004@gmail.com>
2024-02-03 22:42:20 +0200

    Вынес весь функционал из кода страницы в компонент Form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,34 +1,15 @@
-'use client'
-
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './Login.module.scss';
 import Link from 'next/link';
 import LoginForm from '@/components/LoginForm/LoginForm';
 
-export type LoginData = {
-    email: string
-    password: string
-}
 
 const LoginPage = () => {
-    const [loginData, setLoginData] = useState<LoginData>({
-        email: '',
-        password: ''
-    })
-
-    const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault
-        console.log(loginData);
-    }
-
     return (
         <main className={styles.wrapper}>
             <div className={styles.container}>
                 <h1 className={styles.title}>Log in</h1>
-                <LoginForm 
-                    formFunc={handleForm}
-                    formStates={setLoginData}
-                />
+                <LoginForm />
                 <div className={styles.no__acc}>
                     <p>No acc?</p>
                     <Link className={styles.link} href='/auth/register'>Register</Link>
