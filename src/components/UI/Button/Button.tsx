4beb5496b709ede5b50869d3175f605ecde960de
VRomanenko04 <vovasan2004@gmail.com>
2024-02-03 22:41:40 +0200

    Модифицировал UI кнопку

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -6,11 +6,12 @@ type Props = {
     textAnimateClass?: string
     onClickFunc?: React.MouseEventHandler<HTMLButtonElement>
     buttonText: string
+    type?: 'submit' | 'reset' | 'button' | undefined
 }
 
 const Button = (props: Props) => {
     return (
-        <button className={`${styles.UiBtn} ${props.extraClass}`} onClick={props.onClickFunc}>
+        <button className={`${styles.UiBtn} ${props.extraClass}`} onClick={props.onClickFunc} type={props.type}>
             <p className={props.textAnimateClass}>{props.buttonText}</p>
         </button>
     )
