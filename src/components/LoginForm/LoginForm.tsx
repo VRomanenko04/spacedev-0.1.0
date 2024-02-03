ff508ddc5b06be344613f318b7da2eea67a31778
VRomanenko04 <vovasan2004@gmail.com>
2024-02-03 22:43:34 +0200

    Сделал функциональную заглушку на LoginForm

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,20 +1,42 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Input from '../Input/Input'
 import Button from '../UI/Button/Button'
 import styles from './LoginForm.module.scss';
 
-type FormProps = {
-    formFunc: () => void
-}
 
-const LoginForm = ({ formFunc }: FormProps) => {
+const LoginForm = () => {
+    const [loginData, setLoginData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(loginData);
+
+        setLoginData({
+            email: '',
+            password: ''
+        })
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target;
+        setLoginData({
+            ...loginData,
+            [id]: value
+        });
+    }
+
     return (
-        <form className={styles.form} onSubmit={formFunc}>
+        <form className={styles.form} onSubmit={handleForm}>
             <div>
-                <Input type='email' labetText='Email'/>
-                <Input type='password' labetText='Password'/>
+                <Input type='email' inputId='email' labetText='Email' handleChange={handleChange} value={loginData.email}/>
+                <Input type='password' inputId='password' labetText='Password' handleChange={handleChange} value={loginData.password}/>
             </div>
-            <Button buttonText='Submit' extraClass={styles.btn}/>
+            <Button buttonText='Submit' extraClass={styles.btn} type='submit'/>
         </form>
     )
 }
