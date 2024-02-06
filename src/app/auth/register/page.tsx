8cb81f9daf8bdb0ab1491b6e5a2a9e44236159e5
VRomanenko04 <vovasan2004@gmail.com>
2024-02-06 16:00:45 +0200

    Сделал страницу регистрации

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './Register.module.scss';
+import RegisterForm from '@/components/RegisterForm/RegisterForm';
+import Link from 'next/link';
+
+
+const RegisterPage = () => {
+    return (
+        <main className={styles.wrapper}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Registration</h1>
+                <RegisterForm />
+                <div className={styles.no__acc}>
+                    <p>Already have account?</p>
+                    <Link className={styles.link} href='/auth/register'>Log in</Link>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default RegisterPage;
