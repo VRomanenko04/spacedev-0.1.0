7f79f19400d396fa8ea828d0901d9ef7d01a518d
VRomanenko04 <vovasan2004@gmail.com>
2024-02-11 02:30:40 +0200

    Сделал деспетчирезацию данных после login в service LoginUser

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,10 +1,14 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../lib/firebase/firebase';
+import { actions as userAuthActions } from '../lib/redux/features/UserAuth.slice';
+import { Dispatch } from 'redux';
 
-export const LoginUser = async (email: string, password: string) => {
+export const LoginUser = async (dispatch: Dispatch<any>, email: string, password: string) => {
     await signInWithEmailAndPassword(auth, email, password)
         .then(({user}) => {
             console.log(user);
+            // После успешного входа, диспетчеризуем действие userAuth с передачей true в качестве payload
+            dispatch(userAuthActions.userAuth(true));
         })
         .catch((err) => alert(`The email or password was entered incorrectly. Try again please. error: ${err}`));
 }
