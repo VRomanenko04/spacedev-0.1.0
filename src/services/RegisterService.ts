a162ec5b7ab96bd4f6a44cc71b4178cc6520e613
VRomanenko04 <vovasan2004@gmail.com>
2024-02-13 14:54:45 +0200

    Добавил функционал redux в запрос, для открытия доступа пользователю

--- src/services/RegisterService.ts
+++ src/services/RegisterService.ts
@@ -1,7 +1,14 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../lib/firebase/firebase';
+import { Dispatch } from '@reduxjs/toolkit';
+import { actions as userAuthActions } from '../lib/redux/features/UserAuth.slice';
 
-export const RegisterUser = async (email: string, password: string) => {
-    await createUserWithEmailAndPassword(auth, email, password);
-    console.log('User has registered');
+export const RegisterUser = async (dispatch:  Dispatch<any>, email: string, password: string) => {
+    await createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+        // После успешной регистрации, диспетчеризуем действие userAuth с передачей true в качестве payload
+        dispatch(userAuthActions.userAuth(true));
+    }).catch((err: any) => {
+        console.log(`Register error: ${err}`)
+    })
 }
