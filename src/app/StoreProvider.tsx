d9cc5e7c924d20b89d17d29a30abd99f1b9f4e7b
VRomanenko04 <vovasan2004@gmail.com>
2024-02-14 23:25:04 +0200

    Внедрил инициализацию при рендере компонента верхнего уровня

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,9 +1,21 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../lib/redux/store';
+import { initializeIsUserAuth } from '@/lib/redux/features/UserAuth.slice';
 
 export default function StoreProvider({ children }: {children : React.ReactNode}) {
+
+    useEffect(() => {
+        const initializeUserAuth = async () => {
+            const userInitializationAction = initializeIsUserAuth();
+            if (userInitializationAction) {
+                store.dispatch(userInitializationAction);
+            } 
+        }
+
+        initializeUserAuth();
+    }, [])
 
     return <Provider store={store}>{children}</Provider>
 }
