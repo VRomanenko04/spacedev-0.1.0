39da105f93fc90e30327ecb0285a524b019f0b76
VRomanenko04 <vovasan2004@gmail.com>
2024-02-16 01:55:57 +0200

    Написал хук для формы регистрации

---
+++ src/hooks/RegisterForm.hook.ts
@@ -0,0 +1,52 @@
+'use client'
+import { RegisterUser } from '@/services/RegisterService';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+export interface FormValues {
+    username: string;
+    email: string;
+    password: string;
+    confPassword: string;
+}
+
+export interface FormErrors {
+    username?: string;
+    email?: string;
+    password?: string;
+    confPassword?: string;
+}
+
+type ValidateFunction = (values: FormValues) => FormErrors;
+
+const useRegisterForm = (initialState: FormValues, validate: ValidateFunction) => {
+    const [values, setValues] = useState<FormValues>(initialState);
+    const [errors, setErrors] = useState<FormErrors>({});
+
+    const dispatch = useDispatch();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({
+            ...values,
+            [e.target.id]: e.target.value
+        });
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const validationErrors = validate(values);
+        setErrors(validationErrors);
+        if (Object.keys(validationErrors).length === 0) {
+            console.log('Values are valid!');
+            RegisterUser(dispatch, values.email, values.password);
+            setValues(initialState);
+        } else {
+            console.log('Data is invalid');
+            console.log(validationErrors);
+        }
+    };
+
+    return { handleChange, handleSubmit, values, errors, setValues };
+}
+
+export default useRegisterForm;
