7680be0723838900a3b5371a7d9001456cbfdb36
VRomanenko04 <vovasan2004@gmail.com>
2024-02-16 20:59:00 +0200

    Доработал функцию валидации формы регистрации

--- src/components/RegisterForm/RegisterFormValidate.ts
+++ src/components/RegisterForm/RegisterFormValidate.ts
@@ -3,7 +3,7 @@ import { FormErrors } from "@/hooks/RegisterForm.hook";
 
 const passwordRegex = /[A-Z]/;
 
-export const validate = (values: { username: any; email: any; password: any; confPassword: any; }) => {
+export const validateRegister = (values: { username: any; email: any; password: any; confPassword: any; }) => {
     let errors: FormErrors = {};
     // Проверка на пустые поля
     if (!values.username) {
@@ -17,22 +17,23 @@ export const validate = (values: { username: any; email: any; password: any; con
     }
     if (!values.confPassword) {
         errors.confPassword = 'Confirm password is required';
-    } 
+    } else {
+        // Проверка длины пароля
+        if (values.password.length < 8) {
+            errors.password = 'Password must be at least 8 symbols';
+        }
+        // Проверка сложности пароля
+        if (!passwordRegex.test(values.password)) {
+            errors.password = 'Password must contain at least one uppercase letter';
+        }
+    }
+    // Проверка длины Username
+    if (values.username && values.username.length < 4) {
+        errors.username = 'Username must be at least 4 symbols';
+    }
     // Проверка подтверждения пароля
     if (values.password !== values.confPassword) {
         errors.confPassword = 'Passwords do not match';
-    }
-    // Проверка длинны пароля
-    if (values.password.lenght < 8) {
-        errors.password = 'Password must be at least 8 symbols';
-    }
-    // Проверка длинны Username
-    if (values.username.lenght < 4) {
-        errors.username = 'Username must be at least 4 symbols';
-    }
-    // Проверка сложности пароля
-    if (!passwordRegex.test(values.password)) {
-        errors.password = 'Password must contain at least one uppercase letter';
     }
     return errors;
 };
