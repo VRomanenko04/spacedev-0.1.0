eb2be1bf14781ad097b2a27b16156fce0afeb545
VRomanenko04 <vovasan2004@gmail.com>
2024-02-16 20:59:40 +0200

    Добавил отображение ошибок при валидации

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -4,7 +4,7 @@ import styles from './RegisterForm.module.scss';
 import Input from '../Input/Input';
 import Button from '../UI/Button/Button';
 import useRegisterForm from '@/hooks/RegisterForm.hook';
-import { validate } from './RegisterFormValidate';
+import { validateRegister } from './RegisterFormValidate';
 
 const INITIAL_STATE = {
     username: '',
@@ -16,7 +16,7 @@ const INITIAL_STATE = {
 const RegisterForm = () => {
     const { handleChange, handleSubmit, values, errors } = useRegisterForm(
         INITIAL_STATE,
-        validate
+        validateRegister
     );
 
     return (
@@ -24,38 +24,62 @@ const RegisterForm = () => {
             <div className={styles.top__block}>
                 <div className={styles.avatar}></div>
                 <div className={styles.inputs__position}>
-                    <Input 
-                        type='text'
-                        labetText='Username'
-                        inputId='username'
-                        handleChange={handleChange}
-                        value={values.username}
-                        isShort
-                    />
-                    <Input 
-                        type='password'
-                        labetText='Password'
-                        inputId='password'
-                        handleChange={handleChange}
-                        value={values.password}
-                        isShort
-                    />
+                    <div className={styles.input__box}>
+                        <Input 
+                            type='text'
+                            labetText='Username'
+                            inputId='username'
+                            handleChange={handleChange}
+                            value={values.username}
+                            isError={errors.username}
+                            isShort
+                        />
+                        {errors.username && (
+                            <p className={styles.error}>{errors.username}</p>
+                        )}
+                    </div>
+                    <div className={styles.input__box}>
+                        <Input 
+                            type='password'
+                            labetText='Password'
+                            inputId='password'
+                            handleChange={handleChange}
+                            value={values.password}
+                            isError={errors.password}
+                            isShort
+                        />
+                        {errors.password && (
+                            <p className={styles.error}>{errors.password}</p>
+                        )}
+                    </div>
                 </div>
                 <div className={styles.inputs__position}>
-                    <Input 
-                        type='email'
-                        labetText='Email'
-                        inputId='email'
-                        handleChange={handleChange}
-                        value={values.email}
-                    />
-                    <Input 
-                        type='password'
-                        labetText='Confirm password '
-                        inputId='confPassword'
-                        handleChange={handleChange}
-                        value={values.confPassword}
-                    />
+                    <div className={styles.input__box}>
+                        <Input 
+                            type='email'
+                            labetText='Email'
+                            inputId='email'
+                            handleChange={handleChange}
+                            value={values.email}
+                            isError={errors.email}
+                        />
+                        {errors.email && (
+                            <p className={styles.error}>{errors.email}</p>
+                        )}
+                    </div>
+                    <div className={styles.input__box}>
+                        <Input 
+                            type='password'
+                            labetText='Confirm password '
+                            inputId='confPassword'
+                            handleChange={handleChange}
+                            value={values.confPassword}
+                            isError={errors.confPassword}
+                        />
+                        {errors.confPassword && (
+                            <p className={styles.error}>{errors.confPassword}</p>
+                        )}
+                    </div>
                 </div>
             </div>
             <div className={styles.bottom__block}>
