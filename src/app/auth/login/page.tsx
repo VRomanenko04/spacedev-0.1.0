dbb6512092b721190b8b7e8407dbee6ff08bd0e0
VRomanenko04 <vovasan2004@gmail.com>
2024-02-17 23:26:28 +0200

    Внедрил AlertModel в LoginPage

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import styles from './Login.module.scss';
 import Link from 'next/link';
 import LoginForm from '@/components/LoginForm/LoginForm';
+import AlertModal from '@/components/AlertModal/AlertModal';
 
 
 const LoginPage = () => {
+    const [isConfirmed, setIsConformed] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const showModal = () => {
+        setIsVisible(true);
+        const timer = setTimeout(() => {
+            setIsVisible(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    };
+
     return (
         <main className={styles.wrapper}>
             <div className={styles.container}>
                 <h1 className={styles.title}>Log in</h1>
-                <LoginForm />
+                <LoginForm 
+                    setIsConformed={setIsConformed}
+                    showModal={showModal}
+                />
                 <div className={styles.no__acc}>
                     <p>No acc?</p>
                     <Link className={styles.link} href='/auth/register'>Register</Link>
                 </div>
             </div>
+            <AlertModal 
+                isConfirmed={isConfirmed} 
+                messageHead={isConfirmed ? 'Login Confirmed!' : 'Login Error!'} 
+                messageText={isConfirmed ? '' : 'Check your details are correct, please'}
+                isVisible={isVisible}
+            />
         </main>
     )
 }
