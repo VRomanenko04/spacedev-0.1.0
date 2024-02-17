ce12c6ad967d8a804d43b293200551b06338568a
VRomanenko04 <vovasan2004@gmail.com>
2024-02-17 23:28:13 +0200

    Усовершенствовал LoginUser service под AlertModal

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -3,12 +3,17 @@ import { auth } from '../lib/firebase/firebase';
 import { actions as userAuthActions } from '../lib/redux/features/UserAuth.slice';
 import { Dispatch } from 'redux';
 
-export const LoginUser = async (dispatch: Dispatch<any>, email: string, password: string) => {
+export const LoginUser = async (dispatch: Dispatch<any>, email: string, password: string, setIsConformed:React.Dispatch<React.SetStateAction<boolean>>) => {
     await signInWithEmailAndPassword(auth, email, password)
         .then(({user}) => {
             console.log(user);
+            setIsConformed(true);
             // После успешного входа, диспетчеризуем действие userAuth с передачей true в качестве payload
             dispatch(userAuthActions.userAuth(true));
         })
-        .catch((err) => alert(`The email or password was entered incorrectly. Try again please. error: ${err}`));
+        .catch((err) => {
+            setIsConformed(false);
+        });
 }
+
+// The email or password was entered incorrectly. Try again please.
