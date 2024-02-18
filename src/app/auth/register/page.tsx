b524995d700d3bd7b214c8cf54dd84fd19d6391e
VRomanenko04 <vovasan2004@gmail.com>
2024-02-18 23:08:25 +0200

    Добавил логику окна-уведомления, раскидал пропсы, сделал компонент клиентским

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from './Register.module.scss';
 import RegisterForm from '@/components/RegisterForm/RegisterForm';
 import Link from 'next/link';
+import AlertModal from '@/components/AlertModal/AlertModal';
 
 
 const RegisterPage = () => {
+    const [isConfirmed, setIsConformed] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const showModal = () => {
+        setIsVisible(true);
+        const timer = setTimeout(() => {
+            setIsVisible(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    };
+
     return (
         <main className={styles.wrapper}>
             <div className={styles.container}>
                 <h1 className={styles.title}>Registration</h1>
-                <RegisterForm />
+                <RegisterForm 
+                    setIsConformed={setIsConformed}
+                    showModal={showModal}
+                />
                 <div className={styles.no__acc}>
                     <p>Already have account?</p>
                     <Link className={styles.link} href='/auth/login'>Log in</Link>
                 </div>
             </div>
+            <AlertModal 
+                isConfirmed={isConfirmed} 
+                messageHead={isConfirmed ? 'Registration Confirmed!' : 'Registration Error!'} 
+                messageText={isConfirmed ? '' : 'Try again later, please'}
+                isVisible={isVisible}
+            />
         </main>
     )
 }
