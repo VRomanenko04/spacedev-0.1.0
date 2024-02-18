80beec97a05d23c1b27b85c92a10bcc624472d49
VRomanenko04 <vovasan2004@gmail.com>
2024-02-18 23:09:30 +0200

    Добавил передачу состояния запроса с RegisterUser service на уведомление клиенту

--- src/services/RegisterService.ts
+++ src/services/RegisterService.ts
@@ -3,12 +3,14 @@ import { auth } from '../lib/firebase/firebase';
 import { Dispatch } from '@reduxjs/toolkit';
 import { actions as userAuthActions } from '../lib/redux/features/UserAuth.slice';
 
-export const RegisterUser = async (dispatch:  Dispatch<any>, email: string, password: string) => {
+export const RegisterUser = async (dispatch:  Dispatch<any>, email: string, password: string, setIsConformed:React.Dispatch<React.SetStateAction<boolean>>) => {
     await createUserWithEmailAndPassword(auth, email, password)
     .then(() => {
+        setIsConformed(true);
         // После успешной регистрации, диспетчеризуем действие userAuth с передачей true в качестве payload
         dispatch(userAuthActions.userAuth(true));
     }).catch((err: any) => {
-        console.log(`Register error: ${err}`)
+        console.log(`Register error: ${err}`);
+        setIsConformed(false);
     })
 }
