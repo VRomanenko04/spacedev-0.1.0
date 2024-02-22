714f9fb25123ba47b61a3242bd3a8e7c65cb9293
VRomanenko04 <vovasan2004@gmail.com>
2024-02-22 21:09:59 +0200

    Написал запрос для добавления пользователя в базу данных

--- src/services/RegisterService.ts
+++ src/services/RegisterService.ts
@@ -2,15 +2,49 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../lib/firebase/firebase';
 import { Dispatch } from '@reduxjs/toolkit';
 import { actions as userAuthActions } from '../lib/redux/features/UserAuth.slice';
+import { get, getDatabase, ref, set } from 'firebase/database';
 
-export const RegisterUser = async (dispatch:  Dispatch<any>, email: string, password: string, setIsConformed:React.Dispatch<React.SetStateAction<boolean>>) => {
+// Регистрация
+export const RegisterUser = async (dispatch:  Dispatch<any>, email: string, username: string, password: string, setIsConformed:React.Dispatch<React.SetStateAction<boolean>>) => {
     await createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
+    .then(({ user }) => {
         setIsConformed(true);
         // После успешной регистрации, диспетчеризуем действие userAuth с передачей true в качестве payload
         dispatch(userAuthActions.userAuth(true));
+        RegisterUserToDB(user.uid, username, email)
     }).catch((err: any) => {
         console.log(`Register error: ${err}`);
         setIsConformed(false);
     })
+}
+
+
+// Добавление пользователя в базу данных
+export const RegisterUserToDB = async (uid: string, username: string, email: string) => {
+    const database = getDatabase();
+    const userRef = ref(database, 'users/' + uid);
+    const allUsersRef = ref(database, 'users');
+    let numChildren = 0;
+
+    // Получение userID
+    await get(allUsersRef)
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                const data = snapshot.val();
+                numChildren = data ? Object.keys(data).length : 0;
+                console.log("Количество узлов в узле 'users': " + numChildren);
+            } else {
+                console.log("Узел 'users' не существует.");
+            }
+        }).catch((error) => {
+            console.error("Ошибка при получении данных: " + error);
+        });
+    
+        const userData = {
+            username: username,
+            email: email,
+            userId: numChildren + 1
+        };
+
+        await set(userRef, userData);
 }
