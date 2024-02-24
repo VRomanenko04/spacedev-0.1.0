3cbefbfc14f3481400bcbbeec8953d2448da565a
VRomanenko04 <vovasan2004@gmail.com>
2024-02-24 19:38:17 +0200

    Убрал из StoreProvider инициализацию

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,22 +1,10 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../lib/redux/store';
-import { initializeIsUserAuth } from '@/lib/redux/features/UserAuth.slice';
-import '../lib/firebase/firebase';
+
 
 export default function StoreProvider({ children }: {children : React.ReactNode}) {
-
-    useEffect(() => {
-        const initializeUserAuth = async () => {
-            const userInitializationAction = initializeIsUserAuth();
-            if (userInitializationAction) {
-                store.dispatch(userInitializationAction);
-            } 
-        }
-
-        initializeUserAuth();
-    }, [])
 
     return <Provider store={store}>{children}</Provider>
 }
