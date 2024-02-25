8a23020477491247815c7ae1c448da6e0088b0e0
VRomanenko04 <vovasan2004@gmail.com>
2024-02-25 23:06:38 +0200

    Начал страницу Account

---
+++ src/app/(routes)/account/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './AccountPage.module.scss';
+import AccountPlanView from '@/components/AccountPlanView/AccountPlanView';
+
+
+const Account = () => {
+    return (
+        <main className={styles.wrapper}>
+            <section className={styles.container}>
+                <div className={styles.left_side}>
+                    <h2>Current subscription</h2>
+                    <AccountPlanView />
+                </div>
+                <div className={styles.right_side}>
+
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Account;
