149ae886375eef3f320dd097424c03c3277e5667
VRomanenko04 <vovasan2004@gmail.com>
2024-03-04 13:11:41 +0200

    Сделал редирект с главной страницы если user - Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+'use client';
 import GamePreview from "@/components/GamePreview/GamePreview";
 import HomeHeader from "@/components/HomeHeader/HomeHeader";
 import HomePlans from "@/components/HomePlans/HomePlans";
 import Footer from "@/components/UI/Footer/Footer";
 import WhySpacedev from "@/components/WhySpacedev/WhySpacedev";
+import { RootState } from "@/lib/redux/store";
+import { redirect } from "next/navigation";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  // redirect если user auth
+  const isAuth = useSelector((state: RootState) => state.authReducer.isAuthenticated);
+  if(isAuth) {
+    redirect('/account');
+  } 
+
   return (
     <>
       <header>
