212d1b5bca0eb6f88fa02d65daed7717d7506197
VRomanenko04 <vovasan2004@gmail.com>
2024-03-04 13:12:13 +0200

    Переработал инициализацию, вынес в отдельную функцию

--- src/app/InitializeApp.tsx
+++ src/app/InitializeApp.tsx
@@ -7,38 +7,38 @@ import { initializeIsUserAuth } from '@/lib/redux/features/UserAuth.slice';
 import '../lib/firebase/firebase';
 import { initializeUserData } from '@/lib/redux/features/UserData.slice';
 
+export const initializeApp = async (dispatch: any, userAuth: any) => {
+    // Initialize user auth
+    const userInitializationAction = initializeIsUserAuth();
+    if (userInitializationAction) {
+        dispatch(userInitializationAction);
+    } else {
+        if (userAuth && userAuth.isAuthenticated) {
+            console.error("User initialization failed.");
+        }
+    }
+
+    // Fetch user data if authenticated
+    if (userAuth && userAuth.isAuthenticated) {
+        try {
+            const action = await initializeUserData(userAuth.uid);
+            if (action) {
+                dispatch(action);
+                console.log('Initialization complete');
+            }
+        } catch (error) {
+            console.error("Error while initializing data:", error);
+        }
+    }
+};
 
 const InitializeApp = ({ children }: {children : React.ReactNode}) => {
     const userAuth = useSelector((state: RootState) => state.authReducer);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const initializeApp = async () => {
-            // Initialize user auth
-            const userInitializationAction = initializeIsUserAuth();
-            if (userInitializationAction) {
-                dispatch(userInitializationAction);
-            } else {
-                if (userAuth.isAuthenticated) {
-                    console.error("User initialization failed.");
-                }
-            }
-
-            // Fetch user data if authenticated
-            if (userAuth.isAuthenticated) {
-                try {
-                    const action = await initializeUserData(userAuth.uid);
-                    if (action) {
-                        dispatch(action);
-                    }
-                } catch (error) {
-                    console.error("Error while initializing data:", error);
-                }
-            }
-        };
-
-        initializeApp();
-    }, [userAuth]);
+        initializeApp(dispatch, userAuth);
+    }, [userAuth, dispatch]);
 
     return <>{ children }</>
 }
