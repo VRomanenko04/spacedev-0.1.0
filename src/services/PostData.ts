8f077341595477f9f7a01656fe682a8ef6d2872e
VRomanenko04 <vovasan2004@gmail.com>
2024-03-07 21:59:44 +0200

    Существенно модернезировал postUserData на обновление данных

--- src/services/PostData.ts
+++ src/services/PostData.ts
@@ -1,4 +1,4 @@
-import { getDatabase, ref, set } from "firebase/database";
+import { get, getDatabase, ref, set, update } from "firebase/database";
 
 type DataProps = {
     [key: string]: any;
@@ -9,8 +9,22 @@ export const postUserData = async (uid: string, postData: DataProps) => {
     try {
         const database = getDatabase();
         const userRef = ref(database, 'users/' + uid);
-    
-        await set(userRef, postData);
+
+        // Проверяем, существует ли узел
+        const snapshot = await get(userRef);
+        if (!snapshot.exists()) {
+            // Если узел не существует, создаем его с помощью set()
+            await set(userRef, postData);
+        } else {
+            const updateData: DataProps = {};
+            for (const key in postData) {
+                if (postData.hasOwnProperty(key)) {
+                    updateData[key] = postData[key];
+                }
+            }
+            // Обновляем только те ключи, которые переданы в updateData
+            await update(userRef, updateData);
+        }
     } catch (error) {
         console.error('Ошибка при записи данных:', error);
         throw error;
