d81e9b070444b4cbc2f180abb239a5063d8eee88
VRomanenko04 <vovasan2004@gmail.com>
2024-03-13 21:02:35 +0200

    Встроил компонент подтверждения изменения пароля в профиль

--- src/components/AccountProfileBlock/AccountProfileBlock.tsx
+++ src/components/AccountProfileBlock/AccountProfileBlock.tsx
@@ -8,6 +8,7 @@ import { postUserData } from '@/services/PostData';
 import { updatePassword } from '@/services/UpdatePassword';
 import Pen_icon from '../../assets/Pen_icon.svg';
 import styles from './AccountProfileBlock.module.scss';
+import ResetPasswordModal from '../ResetPasswordModal/ResetPasswordModal';
 
 
 const AccountProfileBlock = () => {
@@ -18,6 +19,7 @@ const AccountProfileBlock = () => {
     const [editableText, setEditableText] = useState(username);
     const [inputValue, setInputValue] = useState('');
     const [isEditing, setIsEditing] = useState(false);
+    const [isPasswordModalActive, setIsPasswordModalActive] = useState(false);
 
     useEffect(() => {
         if (username) {
@@ -48,48 +50,56 @@ const AccountProfileBlock = () => {
     const handleResetPassword = () => {
         if (email)
         updatePassword(email);
+        setIsPasswordModalActive(false);
     }
 
     return (
-        <section className={styles.container}>
-            <div className={styles.title__container} onClick={() => {setIsEditing(true)}}>
-                <h3 className={styles.title}>Profile</h3>
-                {!isEditing && <p className={styles.edit}>Edit <Image className={styles.pen} src={Pen_icon} alt='White pen icon'/></p>}
-            </div>
-            <div className={styles.userinfo__container}>
-                <div className={styles.userinfo__list}>
-                    <p className={styles.username}>
+        <>
+            <section className={styles.container}>
+                <div className={styles.title__container} onClick={() => {setIsEditing(true)}}>
+                    <h3 className={styles.title}>Profile</h3>
+                    {!isEditing && <p className={styles.edit}>Edit <Image className={styles.pen} src={Pen_icon} alt='White pen icon'/></p>}
+                </div>
+                <div className={styles.userinfo__container}>
+                    <div className={styles.userinfo__list}>
+                        <p className={styles.username}>
+                            {isEditing ? (
+                                <input 
+                                    type="text"
+                                    value={inputValue}
+                                    onChange={handleTextChange}
+                                    onKeyDown={handleKeyPress}
+                                    className={styles.username__input}
+                                />
+                            ) : (
+                                <strong>{editableText} </strong>
+                            )}
+                            #{userId}
+                        </p>
+                        <p>{email}</p>
                         {isEditing ? (
-                            <input 
-                                type="text"
-                                value={inputValue}
-                                onChange={handleTextChange}
-                                onKeyDown={handleKeyPress}
-                                className={styles.username__input}
-                            />
+                            <p>Password: <u onClick={() => setIsPasswordModalActive(true)} style={{cursor: 'pointer'}}>change</u></p>
                         ) : (
-                            <strong>{editableText} </strong>
+                            <p>Password: ********</p>
                         )}
-                        #{userId}
-                    </p>
-                    <p>{email}</p>
-                    {isEditing ? (
-                        <p>Password: <u onClick={handleResetPassword} style={{cursor: 'pointer'}}>change</u></p>
-                    ) : (
-                        <p>Password: ********</p>
-                    )}
-                    {isEditing && (
-                        <div className={styles.action_links}>
-                            <p className={styles.delete}><u>Delete account</u></p>
-                            <p className={styles.change} onClick={() => saveNewData()}><u>Save changes</u></p>
-                        </div>
-                    )}
+                        {isEditing && (
+                            <div className={styles.action_links}>
+                                <p className={styles.delete}><u>Delete account</u></p>
+                                <p className={styles.change} onClick={() => saveNewData()}><u>Save changes</u></p>
+                            </div>
+                        )}
+                    </div>
+                    <div className={styles.avatar}>
+                        {/* Компонент выбора аватара */}
+                    </div>
                 </div>
-                <div className={styles.avatar}>
-                    {/* Компонент выбора аватара */}
-                </div>
-            </div>
-        </section>
+            </section>
+            <ResetPasswordModal 
+                handleResetPassword={handleResetPassword}
+                isPasswordModalActive={isPasswordModalActive}
+                setIsPasswordModalActive={setIsPasswordModalActive}
+            />
+        </>
     )
 }
 
